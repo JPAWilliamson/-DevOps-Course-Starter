26c9f3f8afec6b9638ee0e49be157b359bb3ee99
Williamson, Josh <josh.williamson@mclaren.com>
2021-04-26 10:41:58 +0100

    trello api exercise 2

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
-from .data import session_items as sessionitems
 from todo_app.flask_config import Config
-import requests
 import json
 import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,10 +10,11 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    allCards = requests.get('https://api.trello.com/1/boards/602410634db0296b392dc99c/cards/open?key={key}&token={token}'.format(os.environ["TRELLO_KEY"], os.environ["TRELLO_TOKEN"] )).json()
-    items = {}
-    for item in allcards.items():
-        
+    allCards = requests.get('https://api.trello.com/1/boards/602410634db0296b392dc99c/cards/open?key={}&token={}'.format(os.environ["TRELLO_KEY"], os.environ["TRELLO_TOKEN"] )).json()
+    items = []
+    for item in allCards:
+        if item['idList'] == "602410634db0296b392dc99d":
+            items.append(item['name'])
     return render_template('index.html', items=items)
 
 @app.route('/add_item', methods=['POST'])
@@ -35,58 +35,41 @@ def add():
     params=query
     )
 
-    allCards = requests.get('https://api.trello.com/1/boards/602410634db0296b392dc99c/cards/open?key={key}&token={token}'.format(os.environ["TRELLO_KEY"], os.environ["TRELLO_TOKEN"] ))
-    items = allCards.text
+    allCards = requests.get('https://api.trello.com/1/boards/602410634db0296b392dc99c/cards/open?key={}&token={}'.format(os.environ["TRELLO_KEY"], os.environ["TRELLO_TOKEN"] )).json()
+    items = []
+    for item in allCards:
+        if item['idList'] == "602410634db0296b392dc99d":
+            items.append(item['name'])
     return render_template('index.html', items=items)
 
-@app.route('/complete_item', methods=['POST'])
-def add():
-    todoTitle = request.form.get('title')
-    url = "https://api.trello.com/1/cards"
+@app.route('/complete_item', methods=['POSt'])
+def complete():
+    itemId= request.form.get('itemId')
+    url = "https://api.trello.com/1/cards/{}".format(itemId)
 
     query = {
     'key': os.environ["TRELLO_KEY"],
     'token': os.environ["TRELLO_TOKEN"], 
-    'idList': '602410634db0296b392dc99d',
-    'name': todoTitle
+    'idList': '602410634db0296b392dc99f',
     }
 
     response = requests.request(
-    "POST",
+    "PUT",
     url,
     params=query
     )
 
-    allCards = requests.get('https://api.trello.com/1/boards/602410634db0296b392dc99c/cards/open?key={key}&token={token}'.format(os.environ["TRELLO_KEY"], os.environ["TRELLO_TOKEN"] ))
-    items = allCards.text
+    allCards = requests.get('https://api.trello.com/1/boards/602410634db0296b392dc99c/cards/open?key={}&token={}'.format(os.environ["TRELLO_KEY"], os.environ["TRELLO_TOKEN"] )).json()
+    items = []
+    for item in allCards:
+        if item['idList'] == "602410634db0296b392dc99d":
+            items.append(item['name'])
     return render_template('index.html', items=items)
     
 if __name__ == '__main__':
     app.run()
 
 #update a  card with the status change
-
-url = "https://api.trello.com/1/cards/{id}"
-
-headers = {
-   "Accept": "application/json"
-}
-
-query = {
-   'key': os.environ["TRELLO_KEY"],
-   'token': os.environ["TRELLO_TOKEN"], 
-   'idList': '602410634db0296b392dc99f'
-}
-
-response = requests.request(
-   "PUT",
-   url,
-   headers=headers,
-   params=query
-)
-
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
 # class for the item
 
 class todoitem:
